0f42b9a645c589496da023200570c6b63bad1dcf
Danilo Vitolo <danilo.vitolo@gmail.com>
2020-04-18 07:30:58 -0300

    desafio solucionado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoryRepository = getRepository(Category);
+
+    let findCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!findCategory) {
+      findCategory = await categoryRepository.save({
+        title: category,
+      });
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const balance = await transactionsRepository.getBalance();
+
+    if (type === 'outcome') {
+      if (balance.total - value < 0) {
+        throw new AppError('Balance error');
+      }
+    }
+
+    const transaction = transactionsRepository.save({
+      title,
+      value,
+      type,
+      category_id: findCategory.id,
+    });
+
+    return transaction;
   }
 }
 
